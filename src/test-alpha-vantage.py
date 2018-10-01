939006986df7679b460a958a7f8e4bd5f0b1cd9a
anoddnumber <suchanoddnumber@gmail.com>
2018-10-01 16:05:17 -0700

    Update script to save data to src/data
    Added pretty print as well so it is easy to read the files.

--- src/test-alpha-vantage.py
+++ src/test-alpha-vantage.py
@@ -1,7 +1,17 @@
 from alpha_vantage.timeseries import TimeSeries
+import os
+import pprint
 
 ts = TimeSeries(key='S86TTOVAFIWT3D8B')
+ticker = 'GOOG'
 # Get json object with the intraday data and another with  the call's metadata
-data, meta_data = ts.get_intraday('GOOGL')
+data, meta_data = ts.get_daily(ticker, 'full')
 
-print(data)
+pp = pprint.PrettyPrinter()
+
+if not os.path.isdir('data'):
+    os.mkdir('data')
+
+history_file = open('data/' + ticker + '.json', 'w')
+history_file.write(pp.pformat(data))
+history_file.close()
