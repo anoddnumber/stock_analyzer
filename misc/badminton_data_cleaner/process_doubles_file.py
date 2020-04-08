6250219739a9046e5b786f5e3b61fddbb30d24ba
anoddnumber <suchanoddnumber@gmail.com>
2020-04-07 20:42:26 -0700

    Created the initial file to process doubles files

---
+++ misc/badminton_data_cleaner/process_doubles_file.py
@@ -0,0 +1,35 @@
+import csv
+
+
+class ProcessDoublesFile:
+
+    @staticmethod
+    def run(filepath):
+        date = filepath.split(sep=' ')[1]
+        ret = {'date': date}
+        with open(filepath, newline='') as csvfile:
+            reader = csv.reader(csvfile)
+            for row in reader:
+                # print(row)
+                try:
+                    rank = int(row[0])
+                    p1_last_name = row[2]
+                    p1_country = row[5]
+                    p2_last_name = row[7]
+                    points = row[11]
+
+                    name = p1_last_name + '/' + p2_last_name
+                    ret[name] = {
+                        'country': p1_country,
+                        'points': points,
+                        'rank': rank,
+                    }
+
+                    if rank >= 10:
+                        break
+                except ValueError:
+                    continue
+        print(ret)
+        return ret
+
+
