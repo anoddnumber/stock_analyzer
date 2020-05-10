d27a832c97062430d094c1a04580b131feefa376
anoddnumber <suchanoddnumber@gmail.com>
2020-05-09 19:12:23 -0700

    Add time_to_live to all the statements as a parameter. Add exception handling of JSONDecodeError and other unexpected exceptions

--- src/scripts/data_retriever.py
+++ src/scripts/data_retriever.py
@@ -1,6 +1,8 @@
 from clients.FinancialModelingPrepClient import FinancialModelingPrepClient
 from dao.file_storage_dao import FileStorageDAO
 import time
+import datetime
+import json
 
 
 class DataRetriever:
@@ -8,31 +10,31 @@ class DataRetriever:
     MAX_TICKERS = 10  # 10 is the max allowed for a single call
 
     @staticmethod
-    def retrieve_financial_statements(tickers):
-        DataRetriever.retrieve_income_statements(tickers)
-        DataRetriever.retrieve_balance_sheets(tickers)
-        DataRetriever.retrieve_cash_flow_statements(tickers)
+    def retrieve_financial_statements(tickers, time_to_live=0):
+        DataRetriever.retrieve_income_statements(tickers, time_to_live)
+        DataRetriever.retrieve_balance_sheets(tickers, time_to_live)
+        DataRetriever.retrieve_cash_flow_statements(tickers, time_to_live)
 
     @staticmethod
-    def retrieve_income_statements(tickers):
+    def retrieve_income_statements(tickers, time_to_live=0):
         return DataRetriever.retrieve_data(tickers, FileStorageDAO.get_income_statement,
                                            FinancialModelingPrepClient.get_income_statements_batch,
-                                           FileStorageDAO.save_income_statement)
+                                           FileStorageDAO.save_income_statement, time_to_live)
 
     @staticmethod
-    def retrieve_balance_sheets(tickers):
+    def retrieve_balance_sheets(tickers, time_to_live=0):
         return DataRetriever.retrieve_data(tickers, FileStorageDAO.get_balance_sheet,
                                            FinancialModelingPrepClient.get_balance_sheets_batch,
-                                           FileStorageDAO.save_balance_sheet)
+                                           FileStorageDAO.save_balance_sheet, time_to_live)
 
     @staticmethod
-    def retrieve_cash_flow_statements(tickers):
+    def retrieve_cash_flow_statements(tickers, time_to_live=0):
         return DataRetriever.retrieve_data(tickers, FileStorageDAO.get_cash_flow_statement,
                                            FinancialModelingPrepClient.get_cash_flow_statements_batch,
-                                           FileStorageDAO.save_cash_flow_statement)
+                                           FileStorageDAO.save_cash_flow_statement, time_to_live)
 
     @staticmethod
-    def retrieve_data(tickers, get_func, fetch_func, save_func, time_to_live=30 * 24 * 60 * 60):
+    def retrieve_data(tickers, get_func, fetch_func, save_func, time_to_live=0):
         tickers_to_retrieve = []
 
         for ticker in tickers:
@@ -40,7 +42,11 @@ class DataRetriever:
                 info = get_func(ticker)
                 if time.time() - float(info.get('last_updated_date', 0)) >= time_to_live:
                     tickers_to_retrieve.append(ticker)
-            except FileNotFoundError:
+            except (FileNotFoundError, json.decoder.JSONDecodeError):
+                tickers_to_retrieve.append(ticker)
+                continue
+            except BaseException as e:
+                print("Unexpected exception: " + str(e))
                 tickers_to_retrieve.append(ticker)
                 continue
 
@@ -49,8 +55,9 @@ class DataRetriever:
         i = 0
         while i < len(tickers_to_retrieve):
             current_tickers = tickers_to_retrieve[i: i + DataRetriever.MAX_TICKERS]
-            print(current_tickers)
+            print('current tickers: ' + str(current_tickers))
             data = fetch_func(current_tickers)
+            print(data)
 
             if len(data) != len(current_tickers):
                 print('retrieve_income_statements: Data length does not match number of tickers')
@@ -61,6 +68,7 @@ class DataRetriever:
             # save to the database
             for ticker, datum in zip(current_tickers, data):
                 datum['last_updated_date'] = time.time()
+                datum['last_updated_date_human'] = str(datetime.datetime.now())
                 save_func(ticker, datum)
 
             i += DataRetriever.MAX_TICKERS
