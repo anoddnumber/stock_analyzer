05c909bbb49f8eb9796632211adf34026325211a
anoddnumber <suchanoddnumber@gmail.com>
2020-05-09 18:20:41 -0700

    Add additional attributes to the company report

--- src/business_objects/company_report.py
+++ src/business_objects/company_report.py
@@ -4,11 +4,22 @@ class CompanyReport:
 
     TICKER = 'ticker'
 
-    # Big 5
-    RETURN_ON_INVESTED_CAPITAL = 'return_on_invested_capital'
-    REVENUE_GROWTH = 'revenue_growth'
-    EARNINGS_GROWTH = 'earnings_growth'
+    # Big 5 - should be greater than or equal to 10% per year for the last 10 years for each of them
+    # These are in order of importance
+    # Check the 5 year and 1 year marks to make sure it isn't slowing down
+
+    # ROIC is the rate of return a business makes from the money it invests into itself
+    # calculated as net operating profit after tax / (equity + debt)
+    # net operating profit after tax (NOPAT) = net income + interest expenses + depreciation expenses
+    # according to investopedia, NOPAT = net income - dividends
+    # NOPAT = operating income - tax expense (?)
+    RETURN_ON_INVESTED_CAPITAL_10_YEAR = 'return_on_invested_capital_10_year'
+    RETURN_ON_INVESTED_CAPITAL_5_YEAR = 'return_on_invested_capital_5_year'
+    RETURN_ON_INVESTED_CAPITAL_1_YEAR = 'return_on_invested_capital_1_year'
+
     EQUITY_GROWTH = 'equity_growth'
+    EARNINGS_GROWTH = 'earnings_growth'
+    REVENUE_GROWTH = 'revenue_growth'
     CASH_GROWTH = 'cash_growth'
 
     # Other considerations
@@ -16,8 +27,13 @@ class CompanyReport:
     TOTAL_DEBT = 'total_debt'
     MARGIN_OF_SAFETY = 'margin_of_safety'
 
-    available_attributes = frozenset({TICKER, RETURN_ON_INVESTED_CAPITAL, REVENUE_GROWTH,
-                                      EARNINGS_GROWTH, EQUITY_GROWTH, CASH_GROWTH,
+    available_attributes = frozenset({TICKER,
+                                      RETURN_ON_INVESTED_CAPITAL_10_YEAR, RETURN_ON_INVESTED_CAPITAL_5_YEAR,
+                                      RETURN_ON_INVESTED_CAPITAL_1_YEAR,
+                                      EQUITY_GROWTH,
+                                      EARNINGS_GROWTH,
+                                      REVENUE_GROWTH,
+                                      CASH_GROWTH,
                                       HISTORIC_PE, TOTAL_DEBT, MARGIN_OF_SAFETY})
 
     def __init__(self):
