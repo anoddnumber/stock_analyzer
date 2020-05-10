846965b98f655903a2757393f8e6612df4620ed4
anoddnumber <suchanoddnumber@gmail.com>
2020-05-09 18:54:32 -0700

    Fix FinancialModelingPrepClient batch APIs to query individually

--- src/clients/FinancialModelingPrepClient.py
+++ src/clients/FinancialModelingPrepClient.py
@@ -1,8 +1,13 @@
 from urllib.request import urlopen
 import json
+import time
 
 
 class FinancialModelingPrepClient:
+
+    INCOME_STATEMENT = 'income_statement'
+    BALANCE_SHEET = 'balance_sheet'
+    CASH_FLOW_STATEMENT = 'cash_flow_statement'
 
     @staticmethod
     def get_income_statement(ticker):
@@ -10,8 +15,7 @@ class FinancialModelingPrepClient:
 
     @staticmethod
     def get_income_statements_batch(tickers):
-        url = 'https://financialmodelingprep.com/api/v3/financials/income-statement/'
-        return FinancialModelingPrepClient.json_get_financial_statement(tickers, url)
+        return FinancialModelingPrepClient.get_batch(tickers, FinancialModelingPrepClient.INCOME_STATEMENT)
 
     @staticmethod
     def get_balance_sheet(ticker):
@@ -19,8 +23,15 @@ class FinancialModelingPrepClient:
 
     @staticmethod
     def get_balance_sheets_batch(tickers):
-        url = 'https://financialmodelingprep.com/api/v3/financials/balance-sheet-statement/'
-        return FinancialModelingPrepClient.json_get_financial_statement(tickers, url)
+        return FinancialModelingPrepClient.get_batch(tickers, FinancialModelingPrepClient.BALANCE_SHEET)
+
+    @staticmethod
+    def get_batch(tickers, statement_type):
+        ret = []
+        for ticker in tickers:
+            time.sleep(1)
+            ret.append(FinancialModelingPrepClient.json_get_single_financial_statement(ticker, statement_type))
+        return ret
 
     @staticmethod
     def get_cash_flow_sheet(ticker):
@@ -28,8 +39,7 @@ class FinancialModelingPrepClient:
 
     @staticmethod
     def get_cash_flow_statements_batch(tickers):
-        url = 'https://financialmodelingprep.com/api/v3/financials/cash-flow-statement/'
-        return FinancialModelingPrepClient.json_get_financial_statement(tickers, url)
+        return FinancialModelingPrepClient.get_batch(tickers, FinancialModelingPrepClient.CASH_FLOW_STATEMENT)
 
     @staticmethod
     def get_tickers():
@@ -48,15 +58,40 @@ class FinancialModelingPrepClient:
         return json.loads(data)
 
     @staticmethod
+    def json_get_single_financial_statement(ticker, statement_type):
+        if statement_type == 'income_statement':
+            url = 'https://financialmodelingprep.com/api/v3/financials/income-statement/'
+        elif statement_type == FinancialModelingPrepClient.BALANCE_SHEET:
+            url = 'https://financialmodelingprep.com/api/v3/financials/balance-sheet-statement/'
+        elif statement_type == 'cash_flow_statement':
+            url = 'https://financialmodelingprep.com/api/v3/financials/cash-flow-statement/'
+
+        url = url + ticker
+        print('url ' + url)
+
+        data = FinancialModelingPrepClient.json_get(url)
+        print('data: ' + str(data))
+
+        # try:
+        #     data = data['financialStatementList']
+        # except KeyError:
+        #     data = [data]
+        return data
+
+    @staticmethod
     def json_get_financial_statement(tickers, url):
         if len(tickers) <= 0:
             return
 
-        arguments = tickers[0]
-        for i in range(1, len(tickers)):
+        # arguments = tickers[0]
+        # for i in range(1, len(tickers)):
+        #     arguments += ',' + tickers[i]
+        arguments = tickers[len(tickers) - 1]
+        for i in range(len(tickers) - 2, -1, -1):
             arguments += ',' + tickers[i]
 
         url = url + arguments
+        print('url ' + url)
         data = FinancialModelingPrepClient.json_get(url)
 
         try:
