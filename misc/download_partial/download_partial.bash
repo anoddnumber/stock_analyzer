73c3f3693a3724464d94849b8a8811eb588f2b85
anoddnumber <suchanoddnumber@gmail.com>
2020-09-26 13:28:28 -0700

    Fix issue with getting multiple clips from the same video where the video title had forward slashes

--- misc/download_partial/download_partial.bash
+++ misc/download_partial/download_partial.bash
@@ -3,31 +3,32 @@
 # Some reason the downloaded video will have a 10 second delay or so from the start time..
 
 url=$1
-start_time=$2 # format of time should be something like this: 00:00:10
+start_time=$2 # format of time should be something like this: 01:22:10 (HH:MM:SS)
 end_time=$3
 
 original_output_name="$(youtube-dl --get-title "$url")"
-output_name="$(youtube-dl --get-title "$url").mp4"
 
 # replace all forward slashes with an underscore, otherwise ffmpeg will treat it as a path
-output_name=$(echo "$output_name"| tr / _)
+valid_name=$(echo ${original_output_name} | tr / _)
+output_name="${valid_name}.mp4"
 
 counter=
 
 # As long as the output_name exists, increment a counter and change the output_name
-while [ -f "$output_name" ]
+while [[ -f "${output_name}" ]]
 do
-    if [ -z $counter ]
+    if [[ -z ${counter} ]]
     then
         counter=1
     else
-        counter=$(($counter+1))
+        counter=$((${counter}+1))
     fi
-    output_name="$original_output_name-$counter.mp4"
+    output_name="${valid_name}-${counter}.mp4"
 done
 
-echo $output_name
+echo ${output_name}
 
+# -f 22 indicates 720p with video and audio. Some reason 1080p only comes with video for BWF videos...
 ffmpeg -i $(youtube-dl -f 22 --get-url "$url") \
--ss $start_time -to $end_time -c:v copy -c:a copy \
-"$output_name" > /dev/null 2>&1
+-ss ${start_time} -to ${end_time} -c:v copy -c:a copy \
+"${output_name}" > /dev/null 2>&1
