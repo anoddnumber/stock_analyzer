9bf9dc8d6488b4636ee56f8545cc2c2101e6d992
Samson <suchanoddnumber@gmail.com>
2023-09-16 01:13:15 -0700

    Fix download issues, use ffmpeg 3 times to get a well formed file

--- misc/download_partial/download_partial.bash
+++ misc/download_partial/download_partial.bash
@@ -9,7 +9,8 @@ original_output_name="$(youtube-dl -f 22 --get-title "$url")"
 
 # replace all forward slashes with an underscore, otherwise ffmpeg will treat it as a path
 valid_name=$(echo ${original_output_name} | tr / _)
-output_name="${valid_name}.mp4"
+#output_name="${valid_name}.mp4"
+output_name="${valid_name}"
 
 counter=
 
@@ -22,13 +23,38 @@ do
     else
         counter=$((${counter}+1))
     fi
-    output_name="${valid_name}-${counter}.mp4"
+#    output_name="${valid_name}-${counter}.mp4"
+    output_name="${valid_name}-${counter}"
 done
 
 # youtube-dl --get-url "$url" returns 2 URLs since BWF's video and audio file are separate for 1080p
 videoUrl=$(youtube-dl --get-url "$url" | head -n 1)
 audioUrl=$(youtube-dl --get-url "$url" | tail -n 1)
 
-ffmpeg -i ${videoUrl} -i ${audioUrl} \
--ss ${start_time} -to ${end_time} -c:v copy -c:a copy \
-"${output_name}"
+videoName="${output_name}-video.mp4"
+audioName="${output_name}-audio.mp4"
+output_name="${output_name}.mp4"
+
+# There were issues with the audio (unknown reason) when trying to combine the video and audio all in the same ffmpeg command
+# So, I split it up into 3 commands - 1 for the video and 1 for the audio and then merged them together with 1 more ffmpeg command
+
+ffmpeg -i ${audioUrl} \
+-ss ${start_time} -to ${end_time} -c:a copy \
+"${audioName}"
+
+ffmpeg -i ${videoUrl} \
+-ss ${start_time} -to ${end_time} -c:v copy \
+"${videoName}"
+
+#ffmpeg -i "./${videoName}" -i "./${audioUrl}" \
+#-ss ${start_time} -to ${end_time} -c:v copy -c:a copy \
+#"${output_name}"
+
+ffmpeg -i "${videoName}" -i "${audioName}" -c:v copy -c:a copy "${output_name}"
+
+#ffmpeg -i ${videoUrl} -i ${audioUrl} \
+#-ss ${start_time} -to ${end_time} -c:v copy -c:a copy \
+#"${output_name}"
+
+# "-c:v copy" means to copy the video without re-encoding
+# "-c:a copy" means to copy the audio without re-encoding
